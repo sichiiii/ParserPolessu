ac251e740635feba2068ceb5f5536d7a1849ad63
sichiiii <76742792+sichiiii@users.noreply.github.com>
2021-02-06 15:58:14 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,50 @@
+import telebot
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait  
 from selenium.webdriver.chrome.options import Options 
 
-group_name = input('Enter ur group: ')
-week_num = int(input('Enter week num(2-18): ')) + 1
+bot = telebot.TeleBot('1605853735:AAGYGN3uWIGJO4MY3vCTMX1qnAjNL80U8UY')
 
-link = f'https://www.polessu.by/ruz/term2/?q={group_name}'
-option = webdriver.ChromeOptions()
-option.add_argument('headless')
-browser = webdriver.Chrome(options=option)
+week_name = 0
+group_name = '0'
 
-browser.get(link)
-browser.find_element_by_xpath('/html/body/section/div/div/div[2]/div/button').click()
-browser.find_element_by_xpath(f'//*[@id="weeks-menu"]/li[{week_num}]/a').click()
+def get_rez(week_name, group_name, message):
+    week_name = week_name+1
+    link = f'https://www.polessu.by/ruz/term2/?q={group_name}'
+    option = webdriver.ChromeOptions()
+    option.add_argument('headless')
+    browser = webdriver.Chrome(options=option)
+    browser.get(link)
+    browser.set_window_size(1000,1200)
+    browser.find_element_by_xpath('/html/body/section/div/div/div[2]/div/button').click()
+    browser.find_element_by_xpath(f'//*[@id="weeks-menu"]/li[{week_name}]/a').click()
+    with open('page.txt', 'w') as f:
+        f.write(browser.page_source)
+        screenshot = browser.save_screenshot("my_screenshot.png")
+    browser.quit()
+    bot.send_photo(message.chat.id, open('/home/jabka/parser/my_screenshot.png', 'rb'))
+    message.text = ''
+    bot.register_next_step_handler(message, start_message)
 
-with open('page.html', 'w') as f:
-    f.write(browser.page_source)
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Hi! Enter group or week(1-18)')
+    bot.register_next_step_handler(message, get_message)
+
+def get_message(message):
+    global week_name
+    global group_name
+    while week_name == 0 or group_name == '0':
+        try:
+            week_name = int(message.text)
+            bot.register_next_step_handler(message, get_message)
+        except Exception:
+            group_name = message.text
+            bot.register_next_step_handler(message, get_message)
+    get_rez(week_name, group_name, message)
+    week_name = 0 
+    group_name = 0
+bot.polling(none_stop=True, interval=0)
+
+
+
