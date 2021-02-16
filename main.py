5d6fbf11f589415d8e0b0042d5b168a9c98ed97d
sichiiii <76742792+sichiiii@users.noreply.github.com>
2021-02-16 11:21:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -58,4 +58,8 @@ def get_message(message):
         week_name = 0 
         group_name = 0
     
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    
 bot.polling(none_stop=True, interval=0)
