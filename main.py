d571e50d25ca7ae7b4122eed96f543a07df03290
sichiiii <76742792+sichiiii@users.noreply.github.com>
2021-02-17 18:09:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -62,6 +62,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
+    bot.set_webhook(url="https://rezpolessu.herokuapp.com/" + tokenBot.TOKEN)
     bot.polling(none_stop=True)
     return "?", 200
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
