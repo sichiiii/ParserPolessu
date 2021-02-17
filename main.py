be0fa7b346d50ed3e66db2b6933c4b374960a532
sichiiii <76742792+sichiiii@users.noreply.github.com>
2021-02-17 17:42:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,30 +9,6 @@ bot = telebot.TeleBot('1605853735:AAGYGN3uWIGJO4MY3vCTMX1qnAjNL80U8UY')
 week_name = 0
 group_name = '0'
 
-def get_rez(week_name, group_name, message):
-    try:
-        link = f'https://www.polessu.by/ruz/term2/?q={group_name}'
-        option = webdriver.ChromeOptions()
-        option.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        option.add_argument('--headless')
-        option.add_argument('--disable-dev-sh-usage')
-        option.add_argument('--disable-gpu')
-        option.add_argument('--no-sandbox')
-        browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=option)
-        
-        browser.get(link)
-        browser.set_window_size(1000,1200)
-        browser.find_element_by_xpath('/html/body/section/div/div/div[2]/div/button').click()
-        browser.find_element_by_xpath(f'//*[@id="weeks-menu"]/li[{week_name}]/a').click()
-
-        screenshot = browser.save_screenshot("my_screenshot.png")
-        bot.send_photo(message.chat.id, open('my_screenshot.png', 'rb'))
-        
-        browser.quit()
-        bot.register_next_step_handler(message, start_message)
-    except Exception:
-        bot.send_message(message.chat.id, 'Oops! Something went wrong! Try another group or week: /start')
-
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Hi! Enter group or week')
@@ -53,20 +29,39 @@ def get_message(message):
         get_rez(week_name, group_name, message)
         week_name = 0 
         group_name = 0
-    
-if "HEROKU" in list(os.environ.keys()):
-    server = Flask(__name__)
-    @server.route("/bot", methods=['POST'])
-    def getMessage():
-        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-        return "!", 200
-    @server.route("/")
-    def webhook():
-        bot.remove_webhook()
-       # bot.set_webhook(url="https://git.heroku.com/rezpolessu.git")
-        bot.polling(none_stop=True)
-        return "?", 200
-    server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
-else:
+
+def get_rez(week_name, group_name, message):
+    try:
+        option = webdriver.ChromeOptions()
+        option.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        option.add_argument('--headless')
+        option.add_argument('--disable-dev-sh-usage')
+        option.add_argument('--disable-gpu')
+        option.add_argument('--no-sandbox')
+        browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=option)
+        
+        link = f'https://www.polessu.by/ruz/term2/?q={group_name}'
+        browser.get(link)
+        browser.set_window_size(1000,1200)
+        browser.find_element_by_xpath('/html/body/section/div/div/div[2]/div/button').click()
+        browser.find_element_by_xpath(f'//*[@id="weeks-menu"]/li[{week_name}]/a').click()
+
+        screenshot = browser.save_screenshot("my_screenshot.png")
+        bot.send_photo(message.chat.id, open('my_screenshot.png', 'rb'))
+        
+        browser.quit()
+        bot.register_next_step_handler(message, start_message)
+    except Exception:
+        bot.send_message(message.chat.id, 'Oops! Something went wrong! Try another group or week: /start')
+        
+server = Flask(__name__)
+@server.route("/bot", methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+@server.route("/")
+def webhook():
     bot.remove_webhook()
     bot.polling(none_stop=True)
+    return "?", 200
+server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
