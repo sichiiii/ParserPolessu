c46b1ea8dd0216632fc92ad372a7a9783db7aa72
sichiiii <76742792+sichiiii@users.noreply.github.com>
2021-02-17 19:08:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -57,11 +57,11 @@ server = Flask(__name__)
 @server.route("/bot", methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    bot.polling(none_stop=True)
     return "!", 200
 @server.route("/")
 def webhook():
     bot.remove_webhook()
+    bot.polling(none_stop=True)
     return "?", 200
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
 
