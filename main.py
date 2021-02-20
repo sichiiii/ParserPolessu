76d9509ba605b79657dd5d0735aeb392d53071c9
sichiiii <76742792+sichiiii@users.noreply.github.com>
2021-02-21 01:02:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import telebot, os, requests
+import telebot, os
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait  
 from selenium.webdriver.chrome.options import Options 
@@ -6,14 +6,17 @@ from flask import Flask
 
 bot = telebot.TeleBot('1605853735:AAGYGN3uWIGJO4MY3vCTMX1qnAjNL80U8UY')
 
+week_name = 0
+group_name = '0'
+
 @bot.message_handler(commands=['start'])
 def startMessage(message):
     bot.send_message(message.chat.id, 'Hi! Enter group or week')
-    getMessage()
+    bot.register_next_step_handler(message, getMessage)
 
-def getMessage():
-    week_name = 0
-    group_name = '0'
+def getMessage(message):
+    global week_name
+    global group_name
     if week_name == 0 or group_name == '0':
         try:
             week_name = int(message.text)
@@ -44,15 +47,12 @@ def getScreen(week_name, group_name, message):
         browser.find_element_by_xpath(f'//*[@id="weeks-menu"]/li[{week_name}]/a').click()
 
         screenshot = browser.save_screenshot("my_screenshot.png")
+        bot.send_photo(message.chat.id, open('my_screenshot.png', 'rb'))
+        
         browser.quit()
-
-        sendPhoto()
     except Exception:
         bot.send_message(message.chat.id, 'Oops! Something went wrong! Try another group or week: /start')
         
-def sendPhoto():
-    bot.send_photo(message.chat.id, open('my_screenshot.png', 'rb'))
-
 server = Flask(__name__)
 @server.route("/bot", methods=['POST'])
 def getMessage():
@@ -64,5 +64,3 @@ def webhook():
     bot.polling(none_stop=True)
     return "?", 200
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
-
-
